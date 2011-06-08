2f219eb1eacae832d5ad1b6c511ff15349b0b02e
rubycs <rubycs@clair.ro>
2011-06-08 16:43:29 +0300

    Features, Post, Teachers done, first round

--- _site/js/jquery.init.js
+++ _site/js/jquery.init.js
@@ -1,19 +1,12 @@
-$(function() {
+jQuery.noConflict();
+     
 
-	// select #flowplanes and make it scrollable. use circular and navigator plugins
-	$("#flowpanes").scrollable({ circular: true, mousewheel: true }).navigator({
-
-		// select #flowtabs to be used as navigator
-		navi: "#flowtabs",
-
-		// select A tags inside the navigator to work as items (not direct children)
-		naviItem: 'a',
-
-		// assign "current" class name for the active A tag inside navigator
-		activeClass: 'current',
-
-		// make browser's back button work
-		history: true
-
-	});
-});
+jQuery(document).ready(function(){
+  
+  // Accordion on features
+  jQuery("#features .link").click(function() {
+    jQuery(this).next().slideToggle(200);
+  });
+     
+    
+}); 
