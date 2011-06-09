e91bba1a6ca395fb6384093a89fb9c225bc009c1
rubycs <rubycs@clair.ro>
2011-06-09 21:24:48 +0300

    Avatar animation added

--- assets/jquery.init.js
+++ assets/jquery.init.js
@@ -19,10 +19,37 @@ jQuery(document).ready(function(){
   );  
   
   // Tooltip in frontpage lessons
-  jQuery("#index #courses li").tipTip({
-    defaultPosition: "top",
-    maxWidth: "300px",
-    edgeOffset: 10
+  jQuery("#index #courses #item.c-1 li, #index #courses #item.c-2 li").tipTip({
+    defaultPosition: "right",
+    maxWidth: "300px"
   });
+  jQuery("#index #courses #item.c-3 li").tipTip({
+    defaultPosition: "left",
+    maxWidth: "300px"
+  });
+  
+  // Move avatars on frontpage
+  jQuery("#index #teacher .img-1").click(function(){
+    var left = jQuery(this).children();
+    var right = jQuery(this).next().children();
+    if (left.attr('style') == '' || left.attr('style') == 'left: 0px;') {
+      left.animate({"left": "+=100px"}, "slow"); 
+      right.animate({"left": "-=100px"}, "slow");    
+    } else {
+      right.animate({"left": "+=100px"}, "slow"); 
+      left.animate({"left": "-=100px"}, "slow");    
+    }    
+  });  
+  jQuery("#index #teacher .img-2").click(function(){
+    var right = jQuery(this).children();
+    var left = jQuery(this).parent().children(".img-1").children();
+    if (right.attr('style') == '' || right.attr('style') == 'left: 0px;') {
+      left.animate({"left": "+=100px"}, "slow"); 
+      right.animate({"left": "-=100px"}, "slow");    
+    } else {
+      right.animate({"left": "+=100px"}, "slow"); 
+      left.animate({"left": "-=100px"}, "slow");    
+    }    
+  }); 
   
 }); 
