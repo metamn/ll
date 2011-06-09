976835c38ee35f4897105b3a0d894005f3f88672
rubycs <rubycs@clair.ro>
2011-06-09 20:04:54 +0300

    Basic hovers and tooltips added

--- assets/jquery.init.js
+++ assets/jquery.init.js
@@ -8,5 +8,21 @@ jQuery(document).ready(function(){
     jQuery(this).next().slideToggle(200);
   });
      
-    
+  // Hover on frontpage images
+  jQuery("#index #features #item").hover(
+    function () {
+      jQuery(this).addClass('highlight');
+    }, 
+    function () {
+      jQuery(this).removeClass('highlight');
+    }
+  );  
+  
+  // Tooltip in frontpage lessons
+  jQuery("#index #courses li").tipTip({
+    defaultPosition: "top",
+    maxWidth: "300px",
+    edgeOffset: 10
+  });
+  
 }); 
